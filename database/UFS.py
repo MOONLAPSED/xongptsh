3a3b30d6b4ac7e9ca2df0da4b6eb6cb7abd3df9b
MOONLAPSED <137737122+MOONLAPSED@users.noreply.github.com>
2023-11-17 02:23:43 -0600

    Update UFS.py

--- database/UFS.py
+++ database/UFS.py
@@ -4,37 +4,6 @@ from main import Kerneltuple_
 logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 
 class UnixFilesystem(Kerneltuple_):
-    def __init__(
-        self,
-        inode,
-        pathname,
-        filetype,
-        permissions,
-        owner,
-        group_id,
-        PID,
-        unit_file,
-        unit_file_addr,
-        size,
-        mtime,
-        atime,
-    ):
-        super().__init__(Kerneltuple_, "Unix filesystem")
-        self.inode = inode
-        self.pathname = pathname
-        self.filetype = filetype
-        self.permissions = permissions
-        self.owner = owner
-        self.group_id = group_id
-        self.PID = PID
-        self.unit_file = unit_file
-        self.unit_file_addr = unit_file_addr
-        self.size = size
-        self.mtime = mtime
-        self.atime = atime
-
-    def __str__(self):
-        return f"{self.inode}: {self.pathname}"
     """
     The UnixFilesystem class represents a Unix filesystem.
 
@@ -67,28 +36,29 @@ class UnixFilesystem(Kerneltuple_):
         mtime,
         atime,
     ):
+        """
+        The constructor for the UnixFilesystem class.
+
+        Parameters:
+        inode (int): The inode of the filesystem.
+        pathname (str): The pathname of the filesystem.
+        filetype (str): The type of the filesystem.
+        permissions (str): The permissions of the filesystem.
+        owner (str): The owner of the filesystem.
+        group_id (str): The group ID of the filesystem.
+        PID (int): The PID of the filesystem.
+        unit_file (str): The unit file of the filesystem.
+        unit_file_addr (str): The address of the unit file.
+        size (int): The size of the filesystem.
+        mtime (int): The modification time of the filesystem.
+        atime (int): The access time of the filesystem.
+        """
         super().__init__(Kerneltuple_, "Unix filesystem")
         self.inode = inode
         self.pathname = pathname
         self.filetype = filetype
         self.permissions = permissions
         self.owner = owner
-    super().__init__(Kerneltuple_, "Unix filesystem")
-    self.inode = inode
-    self.pathname = pathname
-    self.filetype = filetype
-    self.permissions = permissions
-    self.owner = owner
-    self.group_id = group_id
-    self.PID = PID
-    self.unit_file = unit_file
-    self.unit_file_addr = unit_file_addr
-    self.size = size
-    self.mtime = mtime
-    self.atime = atime
-
-    def __str__(self):
-        return f"{self.inode}: {self.pathname}"
         self.group_id = group_id
         self.PID = PID
         self.unit_file = unit_file
